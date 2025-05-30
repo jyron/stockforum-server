1c76dc27a936134771c9e50be20824ba87c5c562
jyron <jyron.dev@gmail.com>
2025-05-30 14:33:06 -0500

    Server build.

--- server.js
+++ server.js
@@ -22,7 +22,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? ["https://stock-forum.netlify.app/"]
+        : ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
